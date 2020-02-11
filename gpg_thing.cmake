869c5921d528b481f0df555f579cc5f65a81af3c
user <email>
2020-02-11 04:34:42 +0000

    cleaning up stuff

--- gpg_thing.cmake
+++ gpg_thing.cmake
@@ -1,10 +1,10 @@
 execute_process(
-  COMMAND_ECHO STDOUT
   COMMAND ${A_INPUT}
   gpg
   ${A_INTERACTIVE}
   ${args}
   RESULT_VARIABLE "${resultcmakesux}"
+  ERROR_QUIET
   ${A_INPUT_FILE}
   ${A_OUTPUT_FILE}
   ${A_OUTPUT_VARIABLE})
