f8fac73e2177290d798b2a5578d4433396432770
user <email>
2020-04-25 06:43:37 +0000

    better example

--- example.cmake
+++ example.cmake
@@ -5,13 +5,14 @@ list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/add_module")
 include(AddModule)
 
 add_module(cstuff
-  GIT master
+  GIT v3
   SIGNER 5F15F8C9C05B4D3D31EBB1C3F66D599380F88521
   file://$ENV{HOME}/repo/cstuff
   file://$ENV{HOME}/code/cstuff
   git@github.com:cyisfor/cstuff.git
   https://github.com/cyisfor/cstuff.git)
-  add_cstuff(record mmapfile)
+
+add_cstuff(record mmapfile)
 
 add_module(sqlite
   FOREIGN
